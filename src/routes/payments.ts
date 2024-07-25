29427caa360d50975c93cbcb56b57de9ad6d6a53
Fabio Fernandes <fabio@fabiofernandes.net>
2024-07-25 17:07:49 -0400

    trying to save a bit of network resources leveraging the client memory

--- src/routes/payments.ts
+++ src/routes/payments.ts
@@ -12,8 +12,7 @@ Payments.post('/', async (req, res) => {
   try {
     const bodySchema = z.object({ code: z.coerce.number(), name: z.string(), amount: z.coerce.number(), grid: z.string().min(100) })
     bodySchema.parse(req.body)
-    await prisma.payment.create({ data: { code: req.body.code, name: req.body.name, amount: req.body.amount, grid: req.body.grid } })
-    const result = await prisma.payment.findMany()
+    const result = await prisma.payment.create({ data: { code: req.body.code, name: req.body.name, amount: req.body.amount, grid: req.body.grid } })    
     res.json({ result })
   } catch (error) {
     if (error instanceof ZodError) {
